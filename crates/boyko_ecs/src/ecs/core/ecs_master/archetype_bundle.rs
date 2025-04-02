dddecd3c1def600b47ee8c57dba40b121ce82789
Celtokisa <bluesteelll@hotmail.com>
2025-04-02 12:38:52 +0300

    ArchetypeBundle skeleton

--- crates/boyko_ecs/src/ecs/core/ecs_master/archetype_bundle.rs
+++ crates/boyko_ecs/src/ecs/core/ecs_master/archetype_bundle.rs
@@ -1,8 +1,10 @@
+use std::ops::{Index, IndexMut};
 use boyko_utils::sparse_map::sparse_map::SparseMap;
 use boyko_utils::identifiers::slot::Slot;
 use boyko_utils::sparse_map::sparse_slot_map::SparseSlotMap;
 use crate::ecs::core::archetype::archetype::Archetype;
 use crate::ecs::core::component::Component;
+use crate::ecs::core::entity::entity::Entity;
 use crate::ecs::identifiers::primitives::{ArchetypeId, InlandArchetypeId};
 
 type EntitySlot = Slot;
@@ -15,5 +17,19 @@ pub struct ArchetypeBundle {
 impl ArchetypeBundle {
     pub fn get_archetype (&self, index: ArchetypeId) {
 
+    }
+}
+
+impl Index for ArchetypeBundle {
+    type Output = Archetype;
+
+    fn index(&self, index: Entity) -> &Self::Output {
+        todo!()
+    }
+}
+
+impl IndexMut for ArchetypeBundle {
+    fn index_mut(&mut self, index: Entity) -> &mut Self::Output {
+        todo!()
     }
 }
